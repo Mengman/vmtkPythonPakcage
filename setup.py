f49ef7d7dae62fd6f0ec83cb53adc402878752c0
Mengman <corrsboy@gmail.com>
2020-08-06 17:01:13 +0800

    add packages name

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     name='vmtk-infervision',
     version='1.4.0',
     author='vmtk author',
-    packages=find_packages(),
+    packages=["vmtk"],
+    package_dir={"vmtk": "vmtk"},
     description="vmtk",
     classifiers=[
         "Programming Language :: Python",
