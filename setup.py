a06483d51184992749512937ea7b0b1fa727731c
Mengman <corrsboy@gmail.com>
2020-08-07 20:44:22 +0800

    update build scripts

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 from skbuild import setup, constants
 from setuptools import find_packages
+import shlex
 
 scripts = [
     "vmtk",
@@ -124,13 +125,16 @@ scripts = [
 
 scripts_paths = [os.path.join("./", constants.CMAKE_INSTALL_DIR(), "bin", s) for s in scripts]
 install_path = os.path.join("./", constants.CMAKE_INSTALL_DIR(), "lib/python3.7/site-packages/vmtk")
+
 setup(
     name='vmtk-infervision',
     version='1.4.0',
     author='vmtk author',
     packages=["vmtk"],
-    scripts=scripts_paths,
-    package_dir={"vmtk": install_path},
+    # scripts=scripts_paths,
+    # package_dir={"vmtk": install_path},
+    package_dir={"vmtk": "vmtk"},
+    cmake_args=shlex.split(os.environ.get('VMTK_CMAKE_ARGS', '')),
     description="vmtk",
     classifiers=[
         "Programming Language :: Python",
